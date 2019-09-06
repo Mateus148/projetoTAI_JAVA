62373e1d28741d4b2d295cb9a89d44ca0137f479
mateusmoura148 <mateusdiasdemoura148@gmail.com>
2019-09-05 22:24:34 -0300

    Adicionando Olá Mundo

--- src/projetotai/ProjetoTAI.java
+++ src/projetotai/ProjetoTAI.java
@@ -15,7 +15,8 @@ public class ProjetoTAI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Olá 4 SI!");
+        System.out.println("Teste");
     }
     
 }
